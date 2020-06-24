4a1f950d1058fca2dd5c3a50a8b8b57662c9dc23
abdullahmohsen <44713320+abdullahmohsen@users.noreply.github.com>
2020-06-24 18:30:29 +0200

    done project

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "Distance.h"
 using namespace std;
 
 int main()
 {
+    Distance d1, d2;
+    d1.setCM(90);
+    d1.setM(1);
+
+    d2.setCM(20);
+    d2.setM(1);
+
+    Distance d3 = d1 + d2;
+    d3.print();
+
+    Distance d4 = ++d3;
+    d4.print();
+
     return 0;
 }
